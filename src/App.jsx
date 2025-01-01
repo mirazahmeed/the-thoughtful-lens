0e92de6affaf10be3ffffcd2c42618982b062d9e
mirazahmeed <mirazahmed0619@gmail.com>
2025-01-01 19:08:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div >
+      <h1 className='text-2xl'>The Thoughtful Lens</h1>
+    </div>
+  )
+}
+
+export default App
