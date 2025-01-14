455ce6ee3f566ee27872c458bf4b5ab8d1c7e708
mirazahmeed <mirazahmed0619@gmail.com>
2025-01-15 02:09:39 +0600

    issues solved

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -3,24 +3,25 @@ import Bookmark from "../Bookmark/Bookmark";
 
 import PropTypes from "prop-types";
 
-const Bookmarks = ({bookmarks, readingTime}) => {
+const Bookmarks = ({ bookmarks, readingTime }) => {
     return (
         <div className="md:w-1/3 shadow-lg space-y-5 p-7">
             <h4 className="text-xl font-semibold italic shadow-lg p-5 rounded-lg bg-slate-300">
                 Reading Time: {readingTime} minutes
             </h4>
-            <h4 className="text-2xl font-semibold italic underline">Bookmarked Blogs: {bookmarks.length}</h4>
-            {
-                bookmarks.map((bookmark) =><Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
-            }
+            <h4 className="text-2xl font-semibold italic underline">
+                Bookmarked Blogs: {bookmarks.length}
+            </h4>
+            {bookmarks.map((bookmark, index) => (
+                <Bookmark key={index} bookmark={bookmark}></Bookmark>
+            ))}
         </div>
     );
 };
 
-
 Bookmarks.propTypes = {
     bookmarks: PropTypes.array.isRequired,
     readingTime: PropTypes.number.isRequired,
-}
+};
 
 export default Bookmarks;
